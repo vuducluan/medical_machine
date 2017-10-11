34e9be15bddfaa2a8ad026ae218ff49347a5cd96
Levitan <vu.duc.luan@framgia.com>
2017-10-11 14:50:34 +0700

    test layout

---
+++ app/assets/javascripts/cherry-popups-scripts.js
@@ -0,0 +1,1 @@
+(function($,CherryJsCore){'use strict';CherryJsCore.utilites.namespace('cherryPopupsFrontScripts');CherryJsCore.cherryPopupsFrontScripts={init:function(){if($('.cherry-popup-wrapper')[0]){$('.cherry-popup-wrapper').cherryPopupsPlugin();}}};CherryJsCore.cherryPopupsFrontScripts.init();}(jQuery,window.CherryJsCore));
--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,14 @@ module MedicalMachine
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.assets.compile = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    # Enable the asset pipeline
+    config.assets.enabled = true
+    config.assets.paths << Rails.root.join('/app/assets/fonts')
   end
 end

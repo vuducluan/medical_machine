17362200dd89e41168018abf680780c25961e00d
HUSTToanLH <laihuutoan@gmail.com>
2017-10-23 16:32:33 +0700

    task_09_User_Product_detail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :get_menu_data
+
+  def get_menu_data
+    get_products_anphabet
+    @fields = Field.all.limit Settings.limit.product_field
+    get_brands
+  end
+
+  private
+  def get_products_anphabet
+    @product_array = []
+    (97..122).each do |anphabet|
+      product = Category.by_anphabet(anphabet.chr)
+      slice_size = (product.size/Settings.limit.menu_block.a_to_z).ceil
+      next unless product.present?
+      @product_array << product.each_slice(slice_size).to_a
+    end
+  end
+
+  def get_brands
+    brands = Brand.all
+    slice_size = (brands.size/Settings.limit.menu_block.brand).ceil
+    @brand_array = brands.each_slice(slice_size).to_a
+  end
 end
--- db/migrate/20171024072808_create_products.rb
+++ db/migrate/20171024072808_create_products.rb
@@ -1,17 +1,16 @@
 class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
+      t.references :brand, foreign_key: true
+      t.references :label, foreign_key: true
       t.string :name
-      t.text :model
-      t.integer :brand_id
+      t.string :model
       t.string :location
       t.integer :price
       t.integer :discount_price
-      t.integer :product_category_id
       t.string :product_type
       t.text :description
-      t.integer :product_field_id
-      t.string :label
+      t.string :short_description
       t.integer :label_order
       t.integer :category_order
 

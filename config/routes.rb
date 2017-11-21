a6adf87a41e319e252fb3b520eccf6be8a88cea7
HUSTToanLH <laihuutoan@gmail.com>
2017-11-21 18:17:49 +0700

    task_21_admin_category

--- config/routes.rb
+++ config/routes.rb
@@ -22,11 +22,12 @@ Rails.application.routes.draw do
     resources :sliders
     resources :catalogs
     resources :products
+    resources :categories
     get "/edit_company" => "companies#edit", as: :edit_company
     resources :fields
     resources :medias
     devise_for :admins, :controllers => {:sessions => 'admin/sessions',
       :passwords => 'admin/passwords' }, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
-    resources :blogs  
+    resources :blogs
   end
 end
